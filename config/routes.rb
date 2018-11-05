9cd6ea775229804536052ec035ef3a6a296fae50
RomainRec <romain.recher@gmail.com>
2018-11-05 17:34:56 +0100

    chapter 5 et 6 ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'sessions/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get 'user/new'
-
-  #root 'static_page#home'
-  #get '/help', to: 'static_page#help'
-  #get '/about', to: 'static_page#about'
-  #get '/contact', to: 'static_page#contact'
-  #get 'signup', to: 'users#new'
-  #ressources :users
-
-  #get '/login', to:'session#new'
-  #post '/login', to:'session#create'
-  #delete '/logout', to:'session#destroy'
-
+  get 'users/new'
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
 end
