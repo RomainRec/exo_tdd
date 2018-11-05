554a574dc6d23a1ea8a018862212b38a5a2a7464
RomainRec <romain.recher@gmail.com>
2018-11-05 21:06:56 +0100

    capter 10 ok

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
+gem 'faker',          '1.7.3'
+
 gem 'jquery-rails'
 
 gem 'rails-controller-testing'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,12 +12,16 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-   # Returns the user corresponding to the remember token cookie.
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+  
+  # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
-      raise       # The tests still pass, so this branch is currently untested.
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
@@ -36,7 +40,8 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
-  
+
+  # Forgets a persistent session.
   def forget(user)
     user.forget
     cookies.delete(:user_id)
@@ -48,5 +53,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
